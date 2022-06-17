86dbd47722268169cd262ad03876372c7fa469bd
dBish6 <davidbish2002@hotmail.com>
2022-06-16 22:48:28 -0230

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+/* App.js
+   The Main Cmd line
+
+   To see the list of commands type "app help".
+
+   Authors: David Bishop, Jacob Pritchett,
+   Alex Frizzell
+   Created Date: June 16, 2022
+   Updates:
+
+*/
---
+++ server.js
@@ -0,0 +1,9 @@
+/* Sever.js
+   List of functions for running the node HTTP sever on LocalHost. 
+
+   Authors: David Bishop, Jacob Pritchett,
+   Alex Frizzell
+   Created Date: June 16, 2022
+   Updates:
+
+*/
