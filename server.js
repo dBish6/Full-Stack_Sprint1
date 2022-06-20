d9a7c8114f7d2a3afa94d464ec8384bbb47b7a38
dBish6 <davidbish2002@hotmail.com>
2022-06-20 13:58:11 -0230

    Initial Commit

--- server.js
+++ server.js
@@ -5,5 +5,16 @@
    Alex Frizzell
    Created Date: June 16, 2022
    Updates:
-
+   June 20, 2022, David, Implemented 
 */
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {});
+
+server.listen(port, "localhost", () => {
+  console.log(
+    `Server is running on http://localhost:${port}; press Ctrl-C to terminate...`
+  );
+});
