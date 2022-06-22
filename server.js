4888c50eaac3cd7584ccac694b91d897a0379586
dBish6 <davidbish2002@hotmail.com>
2022-06-22 13:17:16 -0230

    fix

--- server.js
+++ server.js
@@ -58,6 +58,12 @@ const server = http.createServer((req, res) => {
 
     // Put JavaScript one here.
   } else {
+    myEmitter.emit(
+      "log",
+      `${req.method}\t${req.url}\t404: no such file or directory found`,
+      "ERROR",
+      "errLog.log"
+    );
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.write("404 Not Found");
     res.end();
